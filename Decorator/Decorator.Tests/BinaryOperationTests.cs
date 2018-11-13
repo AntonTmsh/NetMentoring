941b98f53c27ed6f203cf9c0642486f2f9bca181
TI-HOME\tmsh_ <ti1@ukr.net>
2018-11-13 11:56:13 +0200

    add tests

---
+++ Decorator/Decorator.Tests/BinaryOperationTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Epam.NetMentoring.Calculator;
+using NUnit.Framework;
+
+namespace Decorator.Tests
+{
+    [TestFixture]
+    public class BinaryOperationTests
+    {
+        [Test]
+        public void ShouldAddTwoNumbers()
+        {
+            var result = new Add(new Const(2), new Const(3)).GetResult();
+
+            Assert.That(result, Is.EqualTo(5));
+        }
+
+        [Test]
+        public void ShouldSubTwoNumbers()
+        {
+            var result = new Sub(new Const(5), new Const(3)).GetResult();
+
+            Assert.That(result, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void ShouldDivideTwoNumbers()
+        {
+            var result = new Divide(new Const(10), new Const(5)).GetResult();
+
+            Assert.That(result, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void ShouldMultiplyTwoNumbers()
+        {
+            var result = new Multiply(new Const(10), new Const(5)).GetResult();
+
+            Assert.That(result, Is.EqualTo(50));
+        }
+
+        [Test]
+        public void ShouldDoSimplyOperations()
+        {
+            var result = new Multiply(new Add(new Const(10), new Const(5)), new Divide(new Sub(new Const(20), new Const(5)), new Const(3))).GetResult();
+
+            Assert.That(result, Is.EqualTo(75));
+        }
+    }
+}
--- Decorator/Decorator/Program.cs
+++ Decorator/Decorator/Program.cs
@@ -6,16 +6,16 @@ using System.Threading.Tasks;
 
 namespace Epam.NetMentoring.Calculator
 {
-    public class Program
-    {
-        static void Main(string[] args)
-        {
-            var res = new Add(new Add(new Const(2), new Const(3)), new Const(5)).GetResult();
-            var test = GetCacheKey(5, 2);
-        }
-        static string GetCacheKey(decimal p1, decimal p2)
-        {
-            return $"{p1}-{p2}";
-        }
-    }
+    //public class Program
+    //{
+    //    //static void Main(string[] args)
+    //    //{
+    //    //    var res = new Add(new Add(new Const(2), new Const(3)), new Const(5)).GetResult();
+    //    //    var test = GetCacheKey(5, 2);
+    //    //}
+    //    //static string GetCacheKey(decimal p1, decimal p2)
+    //    //{
+    //    //    return $"{p1}-{p2}";
+    //    //}
+    //}
 }
